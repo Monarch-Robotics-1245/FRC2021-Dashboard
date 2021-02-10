48af4d19e27cf031feb7979eced05a27a28c6f27
jordanmosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2021-02-10 08:59:06 -0700

    initial testing with custom frc-web-components

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -17,27 +17,29 @@ export class MapComponent{
   maxX = 8.5;
   minY = -2;
   maxY = 2;
-  fieldHeight: 10;
-  fieldWidth: 10;
+  fieldHeight = 10;
+  fieldWidth = 10;
 
   constructor() {
     this.rotation = 0;
     this.x = 0;
     this.y = 0;
+    this.fieldHeight = 100;
+    this.fieldWidth = 100;
+    setInterval( () => {
+      this.updateField();
+    },50)
     NetworkTables.addKeyListener("/Position/r_path",(key,value) => {
       console.log(key, value);
       this.rotation = value;
-      this.updateField();
     }, true);
     NetworkTables.addKeyListener("/Position/x_path",(key,value) => {
       console.log(key, value);
       this.x = value;
-      this.updateField();
     }, true);
     NetworkTables.addKeyListener("/Position/y_path",(key,value) => {
       console.log(key, value);
       this.y = value;
-      this.updateField();
     }, true);
   }
 
@@ -54,7 +56,6 @@ export class MapComponent{
     let yRange = this.maxY - this.minY;
     let x = (this.x - this.minX)/xRange * this.fieldWidth - 75;
     let y = (1 -(this.y - this.minY)/yRange) * this.fieldHeight - 92.1;
-
     return 'top: '+y.toString()+'px; '
       +'left:'+x.toString()+'px;'
       +'transform: rotate('+(this.rotation - 90).toString()+"deg);";
