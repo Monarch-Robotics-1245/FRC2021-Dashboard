dd16edc20a4119652fd2f9b1d3b1c10e51419ac1
jordanmosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2021-02-09 20:28:08 -0700

    removed electron, unnecessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CameraComponent } from './camera/camera.component';
-import { DrivetrainComponent } from './drivetrain/drivetrain.component';
 import {FormsModule} from "@angular/forms";
-import { CompassComponent } from './compass/compass.component';
-import { PdpComponent } from './pdp/pdp.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CameraComponent,
-    DrivetrainComponent,
-    CompassComponent,
-    PdpComponent
   ],
   imports: [
       BrowserModule,
